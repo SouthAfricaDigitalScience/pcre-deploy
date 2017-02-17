8129ccc5a35b40266b5457b4290f6f1a17209014
Bruce Becker <brucellino@gmail.com>
2017-02-17 17:22:44 +0100

    Fix naming convention

--- deploy.sh
+++ deploy.sh
@@ -17,8 +17,8 @@ cmake .. \
   -DBUILD_SHARED_LIBS=ON \
   -DPCRE2_BUILD_PCRE2_16=ON \
   -DPCRE2_BUILD_PCRE2_32=ON \
-  -DZLIB_INCLUDE_DIR=${LIBZ_DIR}/include -DZLIB_LIBRARY_RELEASE=${ZLIB_DIR}/lib/libz.so \
-  -DBZIP2_INCLUDE_DIR=${BZlIB_DIR}/include -DBZIP2_LIBRARY_RELEASE=${BZLIB_DIR}/lib/libbz2.so
+  -DZLIB_INCLUDE_DIR=${ZLIB_DIR}/include -DZLIB_LIBRARY_RELEASE=${ZLIB_DIR}/lib/libz.so \
+  -DBZIP2_INCLUDE_DIR=${BZLIB_DIR}/include -DBZIP2_LIBRARY_RELEASE=${BZLIB_DIR}/lib/libbz2.so
 
 make install
 echo "Creating the modules file directory ${LIBRARIES_MODULES}"
